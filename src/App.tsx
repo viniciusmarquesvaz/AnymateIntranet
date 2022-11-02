1410a938802e04a429fdf5ab16363fe835a73dc3
vini.marques <vinicius.marques@enline-transmission.com>
2022-11-02 14:53:42 -0300

    some adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import './styles/global.css'
+import './styles/global.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
- import { MainFirstPage } from './components/Pages/MainFirstPage';
-import { WavyContainer } from "react-wavy-transitions";
+import { MainFirstPage } from './components/Pages/MainFirstPage';
+import { WavyContainer } from 'react-wavy-transitions';
 
-function App()  {
+function App() {
+	const About = () => <div>About</div>;
+	// const Contact = () => <div>Contact</div>;
+	return (
+		<BrowserRouter>
+			<WavyContainer />
+			<Routes>
+				<Route index element={<MainFirstPage />} />
 
- const About = () => <div>About</div>;
-// const Contact = () => <div>Contact</div>;
-  return(
-    <BrowserRouter>
-    <WavyContainer />
-    <Routes>
-      <Route index element={<MainFirstPage />} />
-    
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<>No Match</>} />
-     
-    </Routes>
-  </BrowserRouter>
-  )
+				<Route path="about" element={<About />} />
+				<Route path="/company1" element={<>Company 1</>} />
+				<Route path="/company2" element={<>Company 2</>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/Pages/MainFirstPage/index.tsx
+++ src/components/Pages/MainFirstPage/index.tsx
@@ -1,47 +1,47 @@
-import React from 'react'
-import '../../../styles/backgroundMainPageStyles/background.layout.css'
-import { BtnEntrace } from '../../Atoms/btnMainEntrace'
-import { BtnEntrace2 } from '../../Atoms/btnMainEntrace2'
-import { JobGifHomePage } from '../../Atoms/gifAnimatedJob'
-import Typed from 'react-typed'
-import { WavyLink } from 'react-wavy-transitions'
+import React from "react";
+import "../../../styles/backgroundMainPageStyles/background.layout.css";
+import { BtnEntrace } from "../../Atoms/btnMainEntrace";
+import { BtnEntrace2 } from "../../Atoms/btnMainEntrace2";
+import { JobGifHomePage } from "../../Atoms/gifAnimatedJob";
+import Typed from "react-typed";
+import { WavyLink } from "react-wavy-transitions";
 
+// let teste = 0;
 
-export const MainFirstPage= ()   => {
+export const MainFirstPage = () => {
   return (
-    <><div className="context">
-      <h1>
-      <Typed
-      strings={[
-            "Welcome to IntranetPage",
-          ]}
-          typeSpeed={50}
-          loop={false}
-          startDelay={10}
-        />
+    <>
+      <div className="context">
+        <h1>
+          <Typed
+            strings={["Welcome to IntranetPage"]}
+            typeSpeed={50}
+            loop={false}
+            startDelay={10}
+          />
         </h1>
-     <p>
-     <Typed
-      strings={[
-        "Choose which of the two environments you want to enter ✏️"
-      ]}
-      typeSpeed={80}
-      backDelay={50}
-      loop={false}
-      /> 
-     </p>
-     <WavyLink  direction="up" to="/company1" color="#534381">
-     <BtnEntrace />       
-      </WavyLink>
-      
-      <WavyLink  to="/company2" color="#534381">
-     <BtnEntrace2 />
-     </WavyLink>
 
-     <JobGifHomePage/>
+        <p>
+          <Typed
+            strings={[
+              "Choose which of the two environments you want to enter ✏️",
+            ]}
+            typeSpeed={80}
+            backDelay={50}
+            loop={false}
+          />
+        </p>
+        <WavyLink to="/company1" color="#534381">
+          <BtnEntrace />
+        </WavyLink>
 
+        <WavyLink to="/company2" color="#534381">
+          <BtnEntrace2 />
+        </WavyLink>
 
-    </div><div className="area">
+        <JobGifHomePage />
+      </div>
+      <div className="area">
         <ul className="circles">
           <li></li>
           <li></li>
@@ -55,6 +55,6 @@ export const MainFirstPage= ()   => {
           <li></li>
         </ul>
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
